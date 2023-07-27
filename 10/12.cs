29f72167a66a78acef81448cc29684b09ba3eb1e
Mitover <76473397+Mitover@users.noreply.github.com>
2023-07-27 19:44:56 +0500

    fix task

--- 10/12.cs
+++ 10/12.cs
@@ -7,7 +7,7 @@ class Program
         int m = Convert.ToInt32(Console.ReadLine());
         int k = Convert.ToInt32(Console.ReadLine());
 
-        if (k < n * m && k % n == 0 || k % m == 0)
+        if (k < n * m && (k % n == 0 || k % m == 0))
             Console.WriteLine("YES");
         else
             Console.WriteLine("NO");
