67be592ee3aa229d80f432b8f09298edab23cf44
Mitover <76473397+Mitover@users.noreply.github.com>
2023-08-06 14:41:55 +0500

    update

--- 5/5.cs
+++ 5/5.cs
@@ -6,8 +6,8 @@ class Program
         string[] numbers = Console.ReadLine().Split(' ');
         int a = int.Parse(numbers[0]);
         int b = int.Parse(numbers[1]);
-        int c = int.Parse(numbers[2]))
+        int n = int.Parse(numbers[2]))
 
-        Console.WriteLine((a * c + b * c / 100) + "rub. " + b * c % 100 + "kop.");
+        Console.WriteLine((a * n + b * n / 100) + "rub. " + b * n % 100 + "kop.");
     }
 }
