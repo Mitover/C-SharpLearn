90a94775eeedd528f2d61f7e64d4d61dcacf5455
Mitover <76473397+Mitover@users.noreply.github.com>
2023-11-02 13:08:28 +0500

    fix file

--- 1/3/4.cs
+++ 1/3/4.cs
@@ -4,9 +4,8 @@ class Program
     static void Main(string[] args)
     {
         int a, b;
-        string[] numbers = Console.ReadLine().Split(' ');
-        a = Convert.ToInt32(numbers[0]);
-        b = Convert.ToInt32(numbers[1]);
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine(a * b);
     }
