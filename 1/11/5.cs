3ae52e4c80d6f5451f6ace81e43d50845856017d
Mitover <76473397+Mitover@users.noreply.github.com>
2023-11-02 14:12:32 +0500

    fix

--- 1/11/5.cs
+++ 1/11/5.cs
@@ -4,7 +4,7 @@ class Program
     static void Main()
     {
         for (int i = 10; i < 26; i++)
-        { // оформите заголовок цикла     
+        { 
             Console.WriteLine(i + " " + (i + 0.4f));
         }
     }
