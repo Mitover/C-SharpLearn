713ca26c30f04032cbfa705aefa99bf6408c70c0
Mitover <76473397+Mitover@users.noreply.github.com>
2023-12-04 17:15:49 +0500

    Update 2.cs

--- 1/11/2.cs
+++ 1/11/2.cs
@@ -4,8 +4,8 @@ class Program
     static void Main()
     {
         for (int i = 20; i <= 30; i++)
-        {  // заголовок цикла   
-            Console.WriteLine(i); // тело цикла   
+        {    
+            Console.WriteLine(i); 
         }
     }
 }
