77b013a77531a5b6284dec937568f9adaf39136f
Mitover <76473397+Mitover@users.noreply.github.com>
2024-01-25 14:32:53 +0500

    Update 7.cs

--- 2/1/7.cs
+++ 2/1/7.cs
@@ -7,5 +7,13 @@ class Program
         int a = Convert.ToInt32(Console.ReadLine());
         int b = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine(word[a] + "" + word[b]);
+
+        // string word = Console.ReadLine();
+        // int number1 = Convert.ToInt32(Console.ReadLine());
+        // int number2 = Convert.ToInt32(Console.ReadLine());
+        // for (int i = number1; i < number2; i++)
+        // {
+        //     Console.Write(word[i]);
+        // }
     }
 }
